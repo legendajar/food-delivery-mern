d71843dff447628617ad42284aeb7965cb10b258
legendajar <harshsolanki989@gmail.com>
2024-07-14 23:03:02 +0530

    style: Add responsive styles for ExploreMenu, Footer, Header, and Navbar components

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState} from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
+import { Link } from 'react-router-dom'
 
 
 const Navbar = () => {
@@ -11,26 +12,26 @@ const Navbar = () => {
     <div className='navbar'>
       <img src={assets.logo} alt="logo" className="navbar-logo" />
       <ul className='navbar-menu'>
-        <li onClick={() => {
+        <Link to="/" onClick={() => {
           setMenu("home")
         }} className={menu==="home"?"active":""}>
           Home
-        </li>
-        <li onClick={() => {
+        </Link>
+        <a href="#explore-menu" onClick={() => {
           setMenu("menu")
         }} className={menu==="menu"?"active":""}>
           Menu
-        </li>
-        <li onClick={() => {
+        </a>
+        <a href="#app-download" onClick={() => {
           setMenu("mobile-app")
         }} className={menu==="mobile-app"?"active":""}>
           Mobile-App
-        </li>
-        <li onClick={() => {
+        </a>
+        <a href="#footer" onClick={() => {
           setMenu("contact-us")
         }} className={menu==="contact-us"?"active":""}>
           Contact Us
-        </li>
+        </a>
       </ul>
       <div className="navbar-right">
           <img src={assets.search_icon} alt="" />

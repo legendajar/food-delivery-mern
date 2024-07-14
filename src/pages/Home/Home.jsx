be5dfcd4f614606b68e6f054b6d8c3cd73cb184c
legendajar <harshsolanki9898@gmail.com>
2024-07-14 10:09:39 +0530

    First Commit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './Home.css'
+import Header from '../../components/Header/Header'
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
+
+const Home = () => {
+  let [category, setCategory] = useState("All");
+  return (
+    <div>
+      <Header />
+      <ExploreMenu category={category} setCategory={setCategory} />
+      <br /><br /><br />
+    </div>
+  )
+}
+
+export default Home
